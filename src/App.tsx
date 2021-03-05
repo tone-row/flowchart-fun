a2aa9d1480a50d4272f90b1e80f10eeca3c061d7
Rob Gordon <rchristopher.gordon@gmail.com>
2021-03-05 14:27:02 -0500

    Better indent for initial text

--- src/App.tsx
+++ src/App.tsx
@@ -44,14 +44,14 @@ const lineColor = "#000000";
 const textColor = "#000000";
 
 const defaultText = `this app works by typing
- new lines create new nodes
-  indentation creates child nodes 
-  and any text: before a colon+space creates a label
- [linking] you can link to nodes using their ID in parentheses
-  like this: (1)
-  lines have a default ID of their line-number
-   but you can also supply a custom ID in brackets
-    like this: (linking) // use single line comments
+  new lines create new nodes
+    indentation creates child nodes 
+    and any text: before a colon+space creates a label
+  [linking] you can link to nodes using their ID in parentheses
+    like this: (1)
+    lines have a default ID of their line-number
+      but you can also supply a custom ID in brackets
+        like this: (linking) // use single line comments
 /*
 or 
 multiline 
