c500cee98da3160fe68b1b8fdfac7609eee8e3b7
Anders andersen <ama@wsdev.dk>
2021-03-30 09:51:19 +0200

    Adjusted zoom behavior

--- app/src/components/Graph.tsx
+++ app/src/components/Graph.tsx
@@ -209,6 +209,9 @@ function Graph({
       userZoomingEnabled: true,
       userPanningEnabled: true,
       boxSelectionEnabled: false,
+      wheelSensitivity: 0.2,
+      minZoom: 0.2,
+      maxZoom: 6
     });
     const cyCurrent = cy.current;
     const errorCyCurrent = errorCy.current;
