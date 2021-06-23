8fce55b0c83a448a4699216456f7ceb3b7c7d2e6
Rob Gordon <rchristopher.gordon@gmail.com>
2021-06-23 16:04:18 -0400

    Only run e2e test in chromium darwin

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -3,6 +3,10 @@ import { PlaywrightTestConfig } from "@playwright/test";
 const config: PlaywrightTestConfig = {
   use: {
     acceptDownloads: true,
+    viewport: {
+      width: 1200,
+      height: 1080,
+    },
   },
 };
 
