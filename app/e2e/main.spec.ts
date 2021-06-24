c8e6bd36fbbc1981a7e161ce6bf379f75310789b
Rob Gordon <rchristopher.gordon@gmail.com>
2021-06-24 12:03:53 -0400

    Tests w/o snapshots

--- app/e2e/main.spec.ts
+++ app/e2e/main.spec.ts
@@ -1,13 +1,15 @@
 import { test, expect } from "@playwright/test";
 const { describe, beforeEach } = test;
 
-const startUrl =
-  process.env.E2E_START_URL ?? "http://localhost:3000?animation=0";
+const startUrl = process.env.E2E_START_URL ?? "http://localhost:3000";
 
 describe("App", () => {
   beforeEach(async ({ page }) => {
-    await page.goto(startUrl);
+    // Use the unanimated version for testing
+    await page.goto(`${startUrl}?animation=0`);
+  });
 
+  test("User can type", async ({ page }) => {
     // Select Text
     await page.click(':nth-match(:text("This app works by typing"), 2)');
 
@@ -24,50 +26,66 @@ describe("App", () => {
     await page.type(".view-lines", `goodbye\n  (test)`);
   });
 
-  // test.skip("User can type", async ({page}) => {})
-  // test.skip("User can change graph type", async ({page}) => {})
-  // test.skip("User can change user settings", async ({page}) => {})
-  // test.skip("User can create a new graph", async ({page}) => {})
+  test("User can change graph type", async ({ page }) => {
+    await page.click('button:has-text("Settings")');
 
-  test("SVG", async ({ page }) => {
-    const [download] = await Promise.all([
-      page.waitForEvent("download"), // wait for download to start
-      page.click('button:has-text("SVG")'),
-    ]);
+    // Click on selector first
+    await page.click(":nth-match(select[name='layout.name'], 2)");
 
-    const stream = await download.createReadStream();
-    const buffer = await streamToBuffer(stream);
-    expect(buffer).toMatchSnapshot("test.svg");
+    // Select circle
+    await page.selectOption(
+      ":nth-match(select[name='layout.name'], 2)",
+      "circle"
+    );
+
+    // Return to Edit screen
+    await page.click('button:has-text("Editor")');
+
+    // Need to wait
+    // Not sure why this isn't instantaneous
+    await new Promise((res) => setTimeout(res, 1000));
+
+    expect(await page.isVisible("text=circle")).toBe(true);
   });
 
-  test("PNG", async ({ page }) => {
-    const [download] = await Promise.all([
-      page.waitForEvent("download"), // wait for download to start
-      page.click('button:has-text("PNG")'),
-    ]);
+  test("User can change user settings", async ({ page }) => {
+    await page.click('button:has-text("Settings")');
 
-    const stream = await download.createReadStream();
-    const buffer = await streamToBuffer(stream);
-    expect(buffer).toMatchSnapshot("test.png");
+    // Set to dark mode
+    await page.click(':nth-match([aria-label="Dark Mode"], 2)');
+
+    // Wait for colors to be replaced
+    await new Promise((res) => setTimeout(res, 500));
+
+    // Expect background to be black
+    const backgroundColor = await page.$eval("body", (e) =>
+      window.getComputedStyle(e).getPropertyValue("background-color")
+    );
+    expect(backgroundColor).toEqual("rgb(15, 15, 15)");
+
+    // Set to french
+    await page.click(':nth-match([aria-label="Select Language: Français"], 2)');
+
+    // Wait for french language to load
+    await new Promise((res) => setTimeout(res, 500));
+
+    // Expect the tab to be in French
+    expect(await page.isVisible('button[role="tab"]:has-text("Éditeur")')).toBe(
+      true
+    );
   });
 
-  test("JPG", async ({ page }) => {
-    const [download] = await Promise.all([
-      page.waitForEvent("download"), // wait for download to start
-      page.click('button:has-text("JPG")'),
-    ]);
+  test("User can create a new graph", async ({ page }) => {
+    // Go to charts
+    await page.click('button[role="tab"]:has-text("Charts")');
+    // Focus chart title
+    await page.click(':nth-match(input[name="chartTitle"], 2)');
+    // Type title
+    await page.type(':nth-match(input[name="chartTitle"], 2)', "My new chart");
 
-    const stream = await download.createReadStream();
-    const buffer = await streamToBuffer(stream);
-    expect(buffer).toMatchSnapshot("test.jpg");
+    // Create
+    await page.press(':nth-match(input[name="chartTitle"], 2)', "Enter");
+    const url = await page.$eval("html", () => window.location.pathname);
+    expect(url).toEqual("/my-new-chart");
   });
 });
-
-function streamToBuffer(stream) {
-  const chunks = [];
-  return new Promise((resolve, reject) => {
-    stream.on("data", (chunk) => chunks.push(Buffer.from(chunk)));
-    stream.on("error", (err) => reject(err));
-    stream.on("end", () => resolve(Buffer.concat(chunks)));
-  });
-}
