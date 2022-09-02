65f918b5f6d900d9ae2f7ad6ae1cd7a6e6011f37
Rob Gordon <rchristopher.gordon@gmail.com>
2022-09-02 13:17:27 -0400

    fix(export): add padding to exported images

--- app/src/components/useDownloadHandlers.ts
+++ app/src/components/useDownloadHandlers.ts
@@ -4,14 +4,17 @@ import { useParams } from "react-router-dom";
 
 import { useBackground, useGraphTheme } from "../lib/graphThemes";
 
+// padding, gets divided in half
+const PADDING = 60;
+
 declare global {
   interface Window {
-    flowchartFunGetSVG: () => Promise<string>;
-    flowchartFunDownloadSVG: () => void;
-    flowchartFunDownloadPNG: () => void;
-    flowchartFunCopyPNG: () => Promise<void>;
-    flowchartFunDownloadJPG: () => void;
-    flowchartFunGetGraphThemeBG: () => string;
+    __FF_getSVG: () => Promise<string>;
+    __FF_downloadSVG: () => void;
+    __FF_downloadPNG: () => void;
+    __FF_copyPNG: () => Promise<void>;
+    __FF_downloadJPG: () => void;
+    __FF_getGraphThemeBG: () => string;
   }
 }
 
@@ -24,151 +27,217 @@ export default function useDownloadHandlers(
   const graphTheme = useGraphTheme();
   const bg = useBackground();
 
-  window.flowchartFunGetSVG = async () => {
-    if (!cy.current) throw new Error("Cytoscape not initialized");
+  useEffect(() => {
+    window.__FF_getSVG = async () => {
+      if (!cy.current) throw new Error("Cytoscape not initialized");
 
-    try {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      const svgStr = cy.current.svg({
-        full: true,
-        scale: 1.5,
-        quality: 1,
-        bg,
-      });
-      const domparser = new DOMParser();
-      const svgEl = domparser.parseFromString(svgStr, "image/svg+xml");
+      try {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const svgStr = cy.current.svg({
+          full: true,
+          scale: 1.5,
+          quality: 1,
+          bg,
+        });
+        const domparser = new DOMParser();
+        const svgEl = domparser.parseFromString(svgStr, "image/svg+xml");
 
-      // Add comment
-      const originalTextComment = svgEl.createComment(
-        `Original Flowchart Text (flowchart.fun):\n\n${textToParse}\n\n`
-      );
+        // TODO: remove if no longer needed in October 2022
+        // Add comment
+        // const originalTextComment = svgEl.createComment(
+        //   `Original Flowchart Text (flowchart.fun):\n\n${textToParse}\n\n`
+        // );
 
-      // Fix Viewbox
-      svgEl.children[0].appendChild(originalTextComment);
-      svgEl.children[0].setAttribute(
-        "viewBox",
-        `0 0 ${svgEl.children[0].getAttribute(
-          "width"
-        )} ${svgEl.children[0].getAttribute("height")}`
-      );
+        // Fix Viewbox
+        // svgEl.children[0].appendChild(originalTextComment);
 
-      // Add font file if necessary
-      let fontString = "";
-      const files = graphTheme.font.files;
-      if (files) {
-        for (const { url, name } of files) {
-          if (!(url in window.__flowchartFunBase64EncodedFonts)) {
-            const fontUrl = `/fonts/${url}`;
-            const font = await fetch(fontUrl)
-              .then((res) => res.arrayBuffer())
-              .catch((e) => console.error(e));
-            if (!font) continue;
-            const base64 = arrayBufferToBase64(font);
-            window.__flowchartFunBase64EncodedFonts[url] = base64;
-          }
-          fontString += `@font-face { font-family: "${name}"; src: url(data:application/x-font-woff2;charset=utf-8;base64,${window.__flowchartFunBase64EncodedFonts[url]}) format("woff2"); }}`;
+        const pad = PADDING / 2;
+        const svgTag = svgEl.children[0];
+        const bgRect = svgTag.querySelector("rect");
+
+        // add an attribute to recognize it
+        if (bgRect) bgRect.setAttribute("data-bg", "");
+
+        // create a group and move all svg children into it
+        const group = svgEl.createElementNS("http://www.w3.org/2000/svg", "g");
+        while (svgTag.firstChild) {
+          group.appendChild(svgTag.firstChild);
         }
+        svgTag.appendChild(group);
+        group.setAttribute("transform", `translate(${pad}, ${pad})`);
+
+        // update width to include padding
+        const w = parseInt(svgTag.getAttribute("width") || "0", 10);
+        const h = parseInt(svgTag.getAttribute("height") || "0", 10);
+        const padWidth = w + PADDING;
+        const padHeight = h + PADDING;
+        svgTag.setAttribute("width", padWidth.toString());
+        svgTag.setAttribute("height", padHeight.toString());
+
+        if (bgRect) {
+          // move bgRect out of group to the top level
+          svgTag.insertBefore(bgRect, group);
+          // make full size
+          bgRect.setAttribute("width", padWidth.toString());
+          bgRect.setAttribute("height", padHeight.toString());
+        }
+
+        // Add font file if necessary
+        let fontString = "";
+        const files = graphTheme.font.files;
+        if (files) {
+          for (const { url, name } of files) {
+            if (!(url in window.__flowchartFunBase64EncodedFonts)) {
+              const fontUrl = `/fonts/${url}`;
+              const font = await fetch(fontUrl)
+                .then((res) => res.arrayBuffer())
+                .catch((e) => console.error(e));
+              if (!font) continue;
+              const base64 = arrayBufferToBase64(font);
+              window.__flowchartFunBase64EncodedFonts[url] = base64;
+            }
+            fontString += `@font-face { font-family: "${name}"; src: url(data:application/x-font-woff2;charset=utf-8;base64,${window.__flowchartFunBase64EncodedFonts[url]}) format("woff2"); }}`;
+          }
+        }
+
+        if (fontString) {
+          const style = document.createElement("style");
+          style.innerHTML = fontString;
+          svgTag.prepend(style);
+        }
+
+        const correctedSvgStr = svgEl.documentElement.outerHTML;
+        const { optimize } = await import("svgo/dist/svgo.browser");
+
+        const { data } = optimize(correctedSvgStr, {
+          js2svg: { pretty: true, indent: 2 },
+          plugins: ["removeDimensions"],
+        });
+        return data;
+      } catch (e) {
+        console.error(e);
+        return "";
       }
+    };
+  }, [bg, cy, graphTheme.font.files]);
 
-      if (fontString) {
-        const style = document.createElement("style");
-        style.innerHTML = fontString;
-        svgEl.children[0].prepend(style);
-      }
-
-      const correctedSvgStr = svgEl.documentElement.outerHTML;
-      const { optimize } = await import("svgo/dist/svgo.browser");
-
-      const { data } = optimize(correctedSvgStr, {
-        js2svg: { pretty: true, indent: 2 },
-        plugins: ["removeDimensions"],
-      });
-
-      return data;
-    } catch (e) {
-      return "";
-    }
-  };
-
-  window.flowchartFunDownloadSVG = async () => {
-    const correctedSvgStr = await window.flowchartFunGetSVG();
-    if (correctedSvgStr)
+  useEffect(() => {
+    window.__FF_downloadSVG = async () => {
+      const svg = await window.__FF_getSVG();
+      if (!svg) return;
       saveAs(
-        new Blob([correctedSvgStr], {
+        new Blob([svg], {
           type: "image/svg+xml;charset=utf-8",
         }),
         `${filename}.svg`
       );
-  };
+    };
+  }, [filename]);
 
-  const downloadPNG = useCallback(() => {
-    if (cy.current) {
-      const pngStr = cy.current.png({
-        full: true,
-        scale: 4,
-        output: "blob",
-        bg,
-      });
-      saveAs(
-        new Blob([pngStr], {
-          type: "image/png",
-        }),
-        `${filename}.png`
-      );
-    }
-    // cy is a ref
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [bg]);
-
-  const copyPNG = useCallback(() => {
-    if (!cy.current) throw new Error("Cytoscape not initialized");
-
-    const pngStr = cy.current.png({
+  const getPNG = useCallback<
+    () => Promise<
+      { canvas: HTMLCanvasElement; cleanup: () => void } | undefined
+    >
+  >(async () => {
+    if (!cy.current) return;
+    const pngStr = await cy.current.png({
       full: true,
-      scale: 4,
-      output: "blob",
+      scale: 3,
+      output: "blob-promise",
       bg,
     });
 
-    return navigator.clipboard.write([
-      new ClipboardItem({
-        [`image/png`]: pngStr,
-      }),
-    ]);
+    // Get width and height of flowchart
+    const { w, h } = await new Promise<{ w: number; h: number }>((resolve) => {
+      try {
+        const img = new Image();
+        img.src = window.URL.createObjectURL(pngStr);
+        let w: number, h: number;
+        img.onload = () => {
+          w = img.width;
+          h = img.height;
+          document.body.removeChild(img);
+          resolve({ w, h });
+        };
+        document.body.appendChild(img);
+      } catch (e) {
+        resolve({ w: 0, h: 0 });
+      }
+    });
 
-    // cy is a ref
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [bg]);
+    // Create canvas with size + padding
+    const canvas = document.createElement("canvas");
+    canvas.width = w + PADDING;
+    canvas.height = h + PADDING;
 
-  const downloadJPG = useCallback(() => {
-    if (cy.current) {
-      const jpgStr = cy.current.jpg({
-        full: true,
-        scale: 4,
-        quality: 1,
-        output: "blob",
-        bg,
-      });
-      saveAs(
-        new Blob([jpgStr], {
-          type: "image/jpg",
-        }),
-        `${filename}.jpg`
-      );
-    }
-    // cy is a ref
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [bg]);
+    // add canvas to document and get context
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
 
-  const getGraphThemeBG = useCallback(() => bg, [bg]);
+    // draw background on canvas
+    ctx.fillStyle = bg;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // take the blob and draw it on center of canvas
+    const img = new Image();
+    img.src = window.URL.createObjectURL(pngStr);
+    return new Promise((resolve) => {
+      img.onload = () => {
+        ctx.drawImage(img, PADDING / 2, PADDING / 2);
+        window.URL.revokeObjectURL(img.src);
+        resolve({
+          canvas,
+          cleanup: () => {
+            // throw away canvas
+            canvas.remove();
+          },
+        });
+      };
+    });
+  }, [bg, cy]);
 
   useEffect(() => {
-    window.flowchartFunDownloadPNG = downloadPNG;
-    window.flowchartFunCopyPNG = copyPNG;
-    window.flowchartFunDownloadJPG = downloadJPG;
-    window.flowchartFunGetGraphThemeBG = getGraphThemeBG;
-  }, [copyPNG, downloadJPG, downloadPNG, getGraphThemeBG]);
+    window.__FF_downloadPNG = async () => {
+      const png = await getPNG();
+      if (!png) return;
+      const { canvas, cleanup } = png;
+      saveAs(canvas.toDataURL("image/png"), `${filename}.png`);
+      cleanup();
+    };
+  }, [filename, getPNG]);
+
+  useEffect(() => {
+    window.__FF_copyPNG = async () => {
+      const png = await getPNG();
+      if (!png) return;
+      const { canvas, cleanup } = png;
+      const blob = await new Promise<Blob | null>((resolve) => {
+        canvas.toBlob((blob) => resolve(blob), "image/png");
+      });
+      if (!blob) return; // TODO: show error
+      cleanup();
+      return navigator.clipboard.write([
+        new ClipboardItem({
+          [`image/png`]: blob,
+        }),
+      ]);
+    };
+  }, [bg, cy, getPNG]);
+
+  useEffect(() => {
+    window.__FF_downloadJPG = async () => {
+      const img = await getPNG();
+      if (!img) return;
+      const { canvas, cleanup } = img;
+      saveAs(canvas.toDataURL("image/jpg"), `${filename}.jpg`);
+      cleanup();
+    };
+  }, [filename, getPNG]);
+
+  useEffect(() => {
+    window.__FF_getGraphThemeBG = () => bg;
+  }, [bg]);
 }
 
 function arrayBufferToBase64(buffer: ArrayBuffer) {
