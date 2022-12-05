645fc0ff99f50ebf2c0a26cbb480a2a7f9ebad85
Rob Gordon <rob.gordon@tone-row.com>
2022-12-05 13:53:43 -0500

    chore(a7s): remove UA4 and optimize in favor of GTM (#383)
    * chore(a7s): remove UA4 and optimize in favor of GTM
    
    * fix(a7s): fix bug when dataLayer not present

--- app/src/components/SharedHeader.tsx
+++ app/src/components/SharedHeader.tsx
@@ -29,7 +29,6 @@ import {
 } from "react";
 import { Link, LinkProps, useLocation } from "react-router-dom";
 
-import { gaChangeTab, gaNewChart } from "../lib/analytics";
 import { useIsValidCustomer } from "../lib/hooks";
 import { useLastChart } from "../lib/useLastChart";
 import { ReactComponent as BrandSvg } from "./brand.svg";
@@ -67,9 +66,6 @@ export const SharedHeader = memo(function SharedHeader() {
                   icon={<Plus height={20} width={20} />}
                   className="shared-header__new"
                   to="/n"
-                  onClick={() => {
-                    gaNewChart();
-                  }}
                 />
               </NavigationMenu.Item>
               <NavigationMenu.Item asChild>
@@ -86,9 +82,6 @@ export const SharedHeader = memo(function SharedHeader() {
                   to="/y"
                   icon={<FolderOpen height={20} width={20} />}
                   aria-current={isChartsPage ? "page" : undefined}
-                  onClick={() => {
-                    gaChangeTab({ action: "navigation" });
-                  }}
                 />
               </NavigationMenu.Item>
               <DropdownMenu.Root>
@@ -109,9 +102,6 @@ export const SharedHeader = memo(function SharedHeader() {
                       icon={<Book height={20} width={20} />}
                       aria-current={isDocsPage ? "page" : undefined}
                       to="/h"
-                      onClick={() => {
-                        gaChangeTab({ action: "help" });
-                      }}
                     />
                   </DropdownMenu.Item>
                   <DropdownMenu.Item asChild>
@@ -120,9 +110,6 @@ export const SharedHeader = memo(function SharedHeader() {
                       icon={<Chat height={20} width={20} />}
                       aria-current={isFeedbackPage ? "page" : undefined}
                       to="/o"
-                      onClick={() => {
-                        gaChangeTab({ action: "feedback" });
-                      }}
                     />
                   </DropdownMenu.Item>
                 </DropdownMenu.Content>
@@ -170,9 +157,6 @@ export const SharedHeader = memo(function SharedHeader() {
                 icon={<Gear height={20} width={20} />}
                 aria-current={isSettingsPage ? "page" : undefined}
                 to="/s"
-                onClick={() => {
-                  gaChangeTab({ action: "settings" });
-                }}
               />
               {isValidCustomer ? (
                 <HeaderClientLink
@@ -180,9 +164,6 @@ export const SharedHeader = memo(function SharedHeader() {
                   icon={<User height={20} width={20} />}
                   aria-current={isAccountPage ? "page" : undefined}
                   to="/a"
-                  onClick={() => {
-                    gaChangeTab({ action: "account" });
-                  }}
                 />
               ) : (
                 <HeaderClientLink
@@ -192,11 +173,12 @@ export const SharedHeader = memo(function SharedHeader() {
                   aria-current={isSponsorPage ? "page" : undefined}
                   onClick={() => {
                     // track in gtm
-                    window.dataLayer.push({
-                      event: "sponsor",
-                      action: "click",
-                      label: "header",
-                    });
+                    if (window?.dataLayer)
+                      window.dataLayer.push({
+                        event: "sponsor",
+                        action: "click",
+                        label: "header",
+                      });
                   }}
                 />
               )}
@@ -338,9 +320,6 @@ function MobileHeader({
               icon={<Plus height={20} width={20} />}
               className="shared-header__new"
               to="/n"
-              onClick={() => {
-                gaNewChart();
-              }}
             />
             <HeaderClientLink
               label={t`Editor`}
@@ -353,9 +332,6 @@ function MobileHeader({
               to="/y"
               icon={<FolderOpen height={20} width={20} />}
               aria-current={isChartsPage ? "page" : undefined}
-              onClick={() => {
-                gaChangeTab({ action: "navigation" });
-              }}
             />
             <HeaderClientLink
               label={t`Documentation`}
@@ -363,9 +339,6 @@ function MobileHeader({
               aria-current={isDocsPage ? "page" : undefined}
               className="mobile-only"
               to="/h"
-              onClick={() => {
-                gaChangeTab({ action: "help" });
-              }}
             />
             <HeaderClientLink
               label={t`Feedback`}
@@ -373,9 +346,6 @@ function MobileHeader({
               aria-current={isFeedbackPage ? "page" : undefined}
               className="mobile-only"
               to="/o"
-              onClick={() => {
-                gaChangeTab({ action: "feedback" });
-              }}
             />
             <HeaderLink
               href="/blog/"
@@ -397,9 +367,6 @@ function MobileHeader({
               icon={<Gear height={20} width={20} />}
               aria-current={isSettingsPage ? "page" : undefined}
               to="/s"
-              onClick={() => {
-                gaChangeTab({ action: "settings" });
-              }}
             />
             {isValidCustomer ? (
               <HeaderClientLink
@@ -407,9 +374,6 @@ function MobileHeader({
                 icon={<User height={20} width={20} />}
                 aria-current={isAccountPage ? "page" : undefined}
                 to="/a"
-                onClick={() => {
-                  gaChangeTab({ action: "account" });
-                }}
               />
             ) : (
               <HeaderClientLink
@@ -419,11 +383,12 @@ function MobileHeader({
                 aria-current={isSponsorPage ? "page" : undefined}
                 onClick={() => {
                   // track event with gtm
-                  window.dataLayer.push({
-                    event: "sponsor",
-                    action: "click",
-                    label: "mobile-header",
-                  });
+                  if (window?.dataLayer)
+                    window.dataLayer.push({
+                      event: "sponsor",
+                      action: "click",
+                      label: "mobile-header",
+                    });
                 }}
               />
             )}
--- app/src/lib/analytics.ts
+++ app/src/lib/analytics.ts
@@ -9,16 +9,6 @@ const gtmId = process.env.REACT_APP_GTM_ID ?? "";
 // hook
 let usePageViews = () => {};
 
-// goals
-let gaChangeGraphOption = (_: { action: string; label: string }) => {};
-let gaChangeTab = (_: { action: string }) => {};
-let gaSponsorCTA = (_: { action: string }) => {};
-let gaCreateChart = (_: { action: string }) => {};
-let gaExportChart = (_: { action: string; label: string }) => {};
-let gaNewChart = () => {};
-let gaUseGraphContextMenu = (_: { action: string }) => {};
-let gaJumpToSponsorPage = (_: { action: string }) => {};
-
 declare global {
   interface Window {
     dataLayer: any[];
@@ -36,78 +26,6 @@ if (gaEnabled && gtmId) {
       });
     }, [location.pathname, location.search]);
   };
-
-  import("react-ga").then((ReactGA) => {
-    ReactGA.initialize("UA-136783019-2");
-
-    gaChangeGraphOption = ({ label, action }) => {
-      ReactGA.event({
-        category: "Change Graph Options",
-        action,
-        label,
-      });
-    };
-
-    gaChangeTab = ({ action }) => {
-      ReactGA.event({
-        category: "Change Tab",
-        action,
-      });
-    };
-
-    gaSponsorCTA = ({ action }) => {
-      ReactGA.event({
-        category: "Sponsor CTA",
-        action,
-      });
-    };
-
-    gaCreateChart = ({ action }) => {
-      ReactGA.event({
-        category: "Create Chart",
-        action,
-      });
-    };
-
-    gaExportChart = ({ action, label }) => {
-      ReactGA.event({
-        category: "Export Chart",
-        action,
-        label,
-      });
-    };
-
-    gaNewChart = () => {
-      ReactGA.event({
-        category: "Home",
-        action: "New Chart",
-      });
-    };
-
-    gaUseGraphContextMenu = ({ action }) => {
-      ReactGA.event({
-        category: "Graph Context Menu",
-        action: action,
-      });
-    };
-
-    gaJumpToSponsorPage = ({ action }) => {
-      ReactGA.event({
-        category: "Jump to Sponsor Page",
-        action,
-      });
-    };
-  });
 }
 
-export {
-  usePageViews,
-  gaChangeGraphOption,
-  gaChangeTab,
-  gaSponsorCTA,
-  gaCreateChart,
-  gaExportChart,
-  gaNewChart,
-  gaUseGraphContextMenu,
-  gaJumpToSponsorPage,
-};
+export { usePageViews };
