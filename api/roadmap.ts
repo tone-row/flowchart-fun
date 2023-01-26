0a3fec23443a0cdcd644c1dc479b5766231a74cb
Rob Gordon <rchristopher.gordon@gmail.com>
2023-01-26 09:58:52 -0500

    chore(research): link research to notion

--- api/roadmap.ts
+++ api/roadmap.ts
@@ -1,7 +1,11 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { getIssues } from "./_lib/_linear";
+import { getAreasOfResearchHtml } from "./_lib/_notion";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const issues = await getIssues();
-  res.status(200).json(issues);
+  const [issues, areasOfResearch] = await Promise.all([
+    getIssues(),
+    getAreasOfResearchHtml(),
+  ]);
+  res.status(200).json({ issues, areasOfResearch });
 }
--- app/src/pages/Roadmap.tsx
+++ app/src/pages/Roadmap.tsx
@@ -12,7 +12,6 @@ export default function Roadmap() {
     staleTime: Infinity,
     suspense: true,
   });
-  console.log(data?.areasOfResearch);
   return (
     <InfoContainer>
       <Box gap={16} content="start" className="slang-type size-0">
@@ -39,35 +38,12 @@ export default function Roadmap() {
           <Type as="h2" size={3} color="color-highlightColor">
             Areas of Research
           </Type>
-          <div className="issues post-content">
-            <div className="issue">
-              <h3>Migrating to Graph Selector Syntax</h3>
-              <div className="issue-description">
-                <p>
-                  Over the next 3-6 months we will migrate to a slightly
-                  different syntax. The reasons are outlined in{" "}
-                  <a href="/posts/graph-syntax-css-selector">this post</a>. The
-                  strategy for migrating will be outlined in an upcoming post.
-                  You can checkout examples of the upcoming syntax here:{" "}
-                  <a href="https://graph-selector-syntax.tone-row.com">
-                    https://graph-selector-syntax.tone-row.com
-                  </a>
-                </p>
-              </div>
-            </div>
-            <div className="issue">
-              <h3>VS Code and PKMS Integration</h3>
-              <div className="issue-description">
-                <p>
-                  The goal is to bring Flowchart Fun syntax and generated graphs
-                  to a VS Code extension as well as to Personal Knowledge
-                  Management Systems (PKMS) like Roam Research and Obsidian.
-                  This was part of the impetus for extracting the syntax into a
-                  separate package.
-                </p>
-              </div>
-            </div>
-          </div>
+          {data && (
+            <div
+              className="issues post-content"
+              dangerouslySetInnerHTML={{ __html: data.areasOfResearch }}
+            />
+          )}
         </Box>
       </Box>
     </InfoContainer>
