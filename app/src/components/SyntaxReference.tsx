908e78c2b0d67190f229fe50a07ae42530d6b18c
Rob Gordon <rchristopher.gordon@gmail.com>
2023-01-30 21:10:52 -0500

    feat(style): add shape options to node ctx menu

--- app/src/components/SyntaxReference.tsx
+++ app/src/components/SyntaxReference.tsx
@@ -264,9 +264,7 @@ export function SyntaxReference() {
                 <Trans>
                   Shapes include rectangle, roundrectangle, ellipse, triangle,
                   pentagon, hexagon, heptagon, octagon, star, barrel, diamond,
-                  vee, rhomboid, polygon, tag, round-rectangle, round-triangle,
-                  round-diamond, round-pentagon, round-hexagon, round-heptagon,
-                  round-octagon, round-tag, cut-rectangle,
+                  vee, rhomboid, polygon, tag, round-rectangle, cut-rectangle,
                   bottom-round-rectangle, and concave-hexagon.
                 </Trans>
               </p>
