ba0d222d1a0698a9ca085076b21e6234b8b446bf
Rob Gordon <rchristopher.gordon@gmail.com>
2023-02-21 10:06:05 -0500

    chore: update sentry

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -24,9 +24,15 @@ window.Buffer = Buffer;
 Sentry.init({
   release: `flowchartfun@${pkg.version}`,
   dsn: "https://5c0087f5d8ae4a6ab7aa4f42eab785f1@o394152.ingest.sentry.io/5673697",
-  integrations: [new Integrations.BrowserTracing()],
   // percentage of transactions to capture for performance monitoring.
   tracesSampleRate: 0.25,
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+  integrations: [new Integrations.BrowserTracing(), new Sentry.Replay()],
   enabled: process.env.REACT_APP_SENTRY_ENABLED === "1",
   environment: process.env.REACT_APP_SENTRY_ENVIRONMENT ?? "development",
 });
