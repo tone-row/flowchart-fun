7db2b966d49591810928c5e81286c05f866ecfc2
Rob Gordon <rchristopher.gordon@gmail.com>
2023-02-21 10:18:33 -0500

    chore: update stripe sdk; log signup errors

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,14 +3,12 @@ import "./slang/slang.css";
 import "core-js/features/object/from-entries";
 import "core-js/features/object/entries";
 
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
 import { Buffer } from "buffer";
 import React from "react";
 import ReactDOM from "react-dom";
 
-import pkg from "../package.json";
 import App from "./components/App";
+import { initSentry } from "./lib/sentry";
 import reportWebVitals from "./reportWebVitals";
 
 // Fixes Webpack 5 Buffer polyfill issue
@@ -21,21 +19,7 @@ declare global {
 }
 window.Buffer = Buffer;
 
-Sentry.init({
-  release: `flowchartfun@${pkg.version}`,
-  dsn: "https://5c0087f5d8ae4a6ab7aa4f42eab785f1@o394152.ingest.sentry.io/5673697",
-  // percentage of transactions to capture for performance monitoring.
-  tracesSampleRate: 0.25,
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
-  integrations: [new Integrations.BrowserTracing(), new Sentry.Replay()],
-  enabled: process.env.REACT_APP_SENTRY_ENABLED === "1",
-  environment: process.env.REACT_APP_SENTRY_ENVIRONMENT ?? "development",
-});
+initSentry();
 
 ReactDOM.render(
   <React.StrictMode>
