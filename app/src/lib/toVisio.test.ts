0dd7b9f4e8f17dabcb0ae14896587c58b293786a
Rob Gordon <rchristopher.gordon@gmail.com>
2023-03-28 15:14:05 -0400

    Fix visio tests

--- app/src/lib/toVisio.test.ts
+++ app/src/lib/toVisio.test.ts
@@ -13,7 +13,7 @@ describe("toVisioFlowchart", () => {
     const graph = parse(`a\nb`);
     const csv = await toVisioFlowchart(graph);
     expect(csv).toBe(
-      `"Process Step ID","Process Step Description","Next Step ID","Connector Label"\n"n1","a","",""\n"n2","b","",""`
+      `"Process Step ID","Process Step Description","Next Step ID","Connector Label","Shape Type"\r\n"n1","a","","","Document"\r\n"n2","b","","","Document"`
     );
   });
 
@@ -21,7 +21,7 @@ describe("toVisioFlowchart", () => {
     const graph = parse(`a\n\tb`);
     const csv = await toVisioFlowchart(graph);
     expect(csv).toBe(
-      `"Process Step ID","Process Step Description","Next Step ID","Connector Label"\n"n1","a","n2",""\n"n2","b","",""`
+      `"Process Step ID","Process Step Description","Next Step ID","Connector Label","Shape Type"\r\n"n1","a","n2","","Start"\r\n"n2","b","","","End"`
     );
   });
 
@@ -29,7 +29,7 @@ describe("toVisioFlowchart", () => {
     const graph = parse(`a\n\tlabel: b`);
     const csv = await toVisioFlowchart(graph);
     expect(csv).toBe(
-      `"Process Step ID","Process Step Description","Next Step ID","Connector Label"\n"n1","a","n2","label"\n"n2","b","",""`
+      `"Process Step ID","Process Step Description","Next Step ID","Connector Label","Shape Type"\r\n"n1","a","n2","label","Start"\r\n"n2","b","","","End"`
     );
   });
 
@@ -37,10 +37,10 @@ describe("toVisioFlowchart", () => {
     const graph = parse(`a\n\tto: b\n\talso to: c`);
     const csv = await toVisioFlowchart(graph);
     expect(csv)
-      .toBe(`"Process Step ID","Process Step Description","Next Step ID","Connector Label"
-"n1","a","n2,n3","to,also to"
-"n2","b","",""
-"n3","c","",""`);
+      .toBe(`"Process Step ID","Process Step Description","Next Step ID","Connector Label","Shape Type"\r
+"n1","a","n2,n3","to,also to","Start"\r
+"n2","b","","","End"\r
+"n3","c","","","End"`);
   });
 
   it.todo("should add all other data attributes");
@@ -57,27 +57,27 @@ describe("toVisioOrgChart", () => {
   it("should include id and label", async () => {
     const graph = parse(`Larry\nCurly\nMoe`);
     const csv = await toVisioOrgChart(graph);
-    expect(csv).toBe(`"Employee ID","Name","Title","Manager ID","Role Type"
-"n1","Larry","","",""
-"n2","Curly","","",""
+    expect(csv).toBe(`"Employee ID","Name","Title","Manager ID","Role Type"\r
+"n1","Larry","","",""\r
+"n2","Curly","","",""\r
 "n3","Moe","","",""`);
   });
 
   it("Should include manager edges", async () => {
     const graph = parse(`Larry\n\tCurly\n\tMoe`);
     const csv = await toVisioOrgChart(graph);
-    expect(csv).toBe(`"Employee ID","Name","Title","Manager ID","Role Type"
-"n1","Larry","","",""
-"n2","Curly","","n1",""
+    expect(csv).toBe(`"Employee ID","Name","Title","Manager ID","Role Type"\r
+"n1","Larry","","",""\r
+"n2","Curly","","n1",""\r
 "n3","Moe","","n1",""`);
   });
 
   it("Should include multiple depths", async () => {
     const graph = parse(`Larry\n\tCurly\n\t\tMoe`);
     const csv = await toVisioOrgChart(graph);
-    expect(csv).toBe(`"Employee ID","Name","Title","Manager ID","Role Type"
-"n1","Larry","","",""
-"n2","Curly","","n1",""
+    expect(csv).toBe(`"Employee ID","Name","Title","Manager ID","Role Type"\r
+"n1","Larry","","",""\r
+"n2","Curly","","n1",""\r
 "n3","Moe","","n2",""`);
   });
 
@@ -86,9 +86,9 @@ describe("toVisioOrgChart", () => {
       `Larry [title=Senior Partner]\n\tCurly [title=Senior Partner]\n\t\ttitle: Moe [title=Junior Partner]`
     );
     const csv = await toVisioOrgChart(graph);
-    expect(csv).toBe(`"Employee ID","Name","Title","Manager ID","Role Type"
-"n1","Larry","Senior Partner","",""
-"n2","Curly","Senior Partner","n1",""
+    expect(csv).toBe(`"Employee ID","Name","Title","Manager ID","Role Type"\r
+"n1","Larry","Senior Partner","",""\r
+"n2","Curly","Senior Partner","n1",""\r
 "n3","Moe","Junior Partner","n2",""`);
   });
 });
