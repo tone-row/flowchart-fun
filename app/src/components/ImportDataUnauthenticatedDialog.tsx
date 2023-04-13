69f23d194e5e0b83996dad749e2044ed306455c4
Rob Gordon <rchristopher.gordon@gmail.com>
2023-04-13 09:35:01 -0400

    Add all translations and autotranslations script

--- app/src/components/ImportDataUnauthenticatedDialog.tsx
+++ app/src/components/ImportDataUnauthenticatedDialog.tsx
@@ -1,12 +1,17 @@
 import { Trans } from "@lingui/macro";
 import * as Dialog from "@radix-ui/react-dialog";
 import { Database, X } from "phosphor-react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import { Content, Overlay } from "../ui/Dialog";
 import { EditorActionTextButton } from "../ui/EditorActionTextButton";
 
 export function ImportDataUnauthenticatedDialog() {
+  useEffect(() => {
+    // preload /images/import-data.png
+    new Image().src = "/images/import-data.png";
+  }, []);
   return (
     <Dialog.Root modal>
       <Dialog.Trigger asChild>
@@ -18,7 +23,7 @@ export function ImportDataUnauthenticatedDialog() {
         <Overlay />
         <Content
           maxWidthClass="max-w-[520px]"
-          className="content-start"
+          className="content-start text-center"
           noPadding
         >
           <img
@@ -26,13 +31,13 @@ export function ImportDataUnauthenticatedDialog() {
             alt="Importing Data from Lucidchart, Google Sheets, and Visio"
             className="rounded-t-lg max-h-[300px] w-full object-cover object-center"
           />
-          <Dialog.Title className="text-lg font-bold flex items-center text-blue-500 bg-background rounded-lg px-4 pl-3 py-3 w-48 justify-center mx-auto -mt-6 relative">
+          <Dialog.Title className="text-lg font-bold inline-flex items-center text-blue-500 bg-background dark:bg-foreground rounded-lg px-4 pl-3 py-3 justify-center mx-auto -mt-6 relative">
             <Database className="mr-2 w-6 h-6" />
             <Trans>Import Data</Trans>
           </Dialog.Title>
           <Dialog.Description asChild>
-            <div className="grid gap-3 p-8 pt-4">
-              <p className="leading-5 text-sm text-neutral-500">
+            <div className="grid gap-3 p-8 pt-4 text-left">
+              <p className="leading-5 text-sm text-neutral-500 dark:text-neutral-400">
                 <Trans>
                   Import data from any CSV file and map it to a new flowchart.
                   This is a great way to import data from other sources like
@@ -43,7 +48,6 @@ export function ImportDataUnauthenticatedDialog() {
                   Fun Pro for just $3/month or $30/year.
                 </Trans>
               </p>
-              <p className="leading-5 text-sm text-neutral-500"></p>
               <Link
                 to="/pricing"
                 className="inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mt-4"
