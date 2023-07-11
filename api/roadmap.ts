997c5ce59f213d49bd1016985a63cfe633648dc8
Rob Gordon <rob.gordon@tone-row.com>
2023-07-10 21:10:59 -0400

    Update Roadmap Page

--- api/roadmap.ts
+++ api/roadmap.ts
@@ -1,11 +1,8 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import { getIssues } from "./_lib/_linear";
+
 import { getAreasOfResearchHtml } from "./_lib/_notion";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const [issues, areasOfResearch] = await Promise.all([
-    getIssues(),
-    getAreasOfResearchHtml(),
-  ]);
-  res.status(200).json({ issues, areasOfResearch });
+  const areasOfResearch = await getAreasOfResearchHtml();
+  res.status(200).json({ areasOfResearch });
 }
