2f38119d645ea1d5bc2ad38c875c1244d58b544c
Rob Gordon <rob.gordon@tone-row.com>
2023-07-10 21:17:39 -0400

    Update Roadmap Page (#542)

--- app/src/pages/Roadmap.tsx
+++ app/src/pages/Roadmap.tsx
@@ -14,22 +14,6 @@ export default function Roadmap() {
   return (
     <Page>
       <InfoHeader title={t`Roadmap`} />
-      {data && data.issues.length > 0 && (
-        <section className="grid gap-5">
-          <SectionTitle>Active Tasks</SectionTitle>
-          <div className="issues post-content">
-            {data.issues.map((issue) => (
-              <div className="issue" key={issue.title}>
-                <h3>{issue.title}</h3>
-                <div
-                  className="issue-description"
-                  dangerouslySetInnerHTML={{ __html: issue.description }}
-                />
-              </div>
-            ))}
-          </div>
-        </section>
-      )}
       <section className="grid gap-5">
         <SectionTitle>Areas of Research</SectionTitle>
         {data && (
@@ -43,15 +27,9 @@ export default function Roadmap() {
   );
 }
 
-type Issue = {
-  title: string;
-  description: string;
-};
-
 async function getRoadmap() {
   const roadmap = await axios.get("/api/roadmap");
   return roadmap.data as {
-    issues: Issue[];
     areasOfResearch: string;
   };
 }
