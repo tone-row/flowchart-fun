0fa060d8ecba0d5d4fe5a99a27e87bc5a802c327
Rob Gordon <rob.gordon@tone-row.com>
2023-07-25 22:32:05 -0400

    Adjust supbase init

--- app/src/lib/supabaseClient.ts
+++ app/src/lib/supabaseClient.ts
@@ -7,8 +7,18 @@ const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 let supabase: SupabaseClient | null = null;
 
-if (supabaseUrl && supabaseAnonKey) {
-  supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
-}
+// if (supabaseUrl && supabaseAnonKey) {
+//   supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+// }
 
 export { supabase };
+
+export function initSupabase() {
+  if (!supabaseUrl || !supabaseAnonKey) {
+    return null;
+  }
+
+  supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+
+  return supabase;
+}
