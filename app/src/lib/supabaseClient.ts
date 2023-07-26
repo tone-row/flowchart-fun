ab40f73061c473bb22cabf4fb6dd5daef2f696b9
Rob Gordon <rob.gordon@tone-row.com>
2023-07-25 22:49:49 -0400

    Remove Logs

--- app/src/lib/supabaseClient.ts
+++ app/src/lib/supabaseClient.ts
@@ -7,14 +7,9 @@ const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 let supabase: SupabaseClient | null = null;
 
-// if (supabaseUrl && supabaseAnonKey) {
-//   supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
-// }
-
 export { supabase };
 
 export function initSupabase() {
-  console.log("init supabase 2");
   if (!supabaseUrl || !supabaseAnonKey) {
     return null;
   }
