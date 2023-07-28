9a7dca06b6af80f9ba5dda2dc5a869e46528b56b
Rob Gordon <rob.gordon@tone-row.com>
2023-07-28 12:07:38 -0400

    Polyfill always

--- app/src/lib/preprocessCytoscapeStyle.ts
+++ app/src/lib/preprocessCytoscapeStyle.ts
@@ -5,10 +5,7 @@ import { devtools } from "zustand/middleware";
 
 import { useUnmountStore } from "./useUnmountStore";
 
-// Dynamically import the polyfill if needed
-if (typeof window.CSSStyleSheet === "undefined") {
-  import("construct-style-sheets-polyfill");
-}
+import "construct-style-sheets-polyfill";
 
 /**
  * Create a zustand store to hold imports requested by styles
