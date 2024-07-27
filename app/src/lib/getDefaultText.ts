51a1fb0159cd35d5241869f1cc5e6169eede908c
Rob Gordon <rob.gordon@tone-row.com>
2024-07-27 13:16:58 -0400

    Update Graph Selector and Other Packages (#695)
    * Upgrade graph-selector version and posthog
    
    * Upgrade to better highlighter
    
    * Fix the load-in of text because it's too slow

--- app/src/lib/getDefaultText.ts
+++ app/src/lib/getDefaultText.ts
@@ -2,16 +2,16 @@ import { t } from "@lingui/macro";
 
 export function getDefaultText() {
   return `${t`Welcome to Flowchart Fun!`}
-  ${t`Get Started`}: ${t`Modify the text to see it transform into a flowchart on the right.`}
-  ${t`Understanding Syntax`} .shape_circle.color_orange
+  ${t`Start`}: ${t`Modify text to see it transform into a flowchart on the right.`}
+  ${t`Understand Syntax`} .shape_circle
     ${t`Begin Typing`}: ${t`Start with a label or decision.`}
-    ${t`Decisions`}: ${t`Use colons, like "Explore options:".`}
-    ${t`Indent for steps`}: ${t`Show progression or dependency.`}
-    ${t`Customize`}: ${t`Add classes to alter color and shape`} \\(.color_blue)
-    ${t`Right-click on nodes to see more options.`}
+    ${t`Decisions`}: ${t`Use colons like "Decisions:".`}
+    ${t`Indent for Steps`}: ${t`Indicate progression or dependency.`}
+    ${t`Customize`}: ${t`Add classes to change color and shape`} \\(.color_red)
+    ${t`Right-click nodes for more options.`}
   ${t`Use AI`} .color_green
-    ${t`Paste a document or outline to auto-convert into a flowchart.`}
+    ${t`Paste a document to convert it into a flowchart.`}
   ${t`Share Your Work`} .color_blue
-    ${t`Download or convert your flowchart via the 'Share' button.`}
+    ${t`Download or share your flowchart using the 'Share' button.`}
 `;
 }
